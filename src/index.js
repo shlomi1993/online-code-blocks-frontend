023dd6520d6b2d46ebd627ea478e1f7057a5d4f0
shlomi1993 <shlomiggl@gmail.com>
2022-12-23 21:22:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
-import reportWebVitals from "./reportWebVitals";
+// import 'bootstrap/dist/css/bootstrap.css'
+// import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
 );
 
-reportWebVitals();
+// reportWebVitals();
